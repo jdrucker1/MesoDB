cbd3a1890f246fa2152220bc0007d6260740f5ad
jdrucker1 <jdrucker1@github.com>
2021-06-11 03:46:47 -0700

    Update mesoDB.py
    Overhaul of design. File structure is now mesoDB/Year/JulianDay/file.pkl. Everything works on a basic level, but limits were not placed (i.e. inputting year > thisYear)

--- mesoDB.py
+++ mesoDB.py
@@ -21,10 +21,12 @@ class mesoDB(object):
     
     # mesoDB constructor
     #
-    def __init__(self, folder_path=osp.join(osp.abspath(os.getcwd()),"mesoDB")):
+    def __init__(self, mesoToken, folder_path=osp.join(osp.abspath(os.getcwd()),"mesoDB")):
         self.folder_path = folder_path
+        self.token = Meso(token=mesoToken)
         self.exists_here()
         self.init_params()
+        
         
        
     # Checks if mesoDB directory exists. If not, it is created
@@ -47,26 +49,29 @@ class mesoDB(object):
         
     # Checks if month folder exists, if not, make it
     #
-    def month_exists(self,year,month):
-        if osp.exists("{}/{:04d}/{:02d}".format(self.folder_path,year,month)):
-            logging.info("mesoDB/year/month - Existent DB path {}/{:04d}/{:02d}".format(self.folder_path,year,month))
+    def julian_exists(self,day,month,year):
+        day = datetime.datetime(year, month, day, tzinfo=datetime.timezone.utc).timetuple().tm_yday
+        if osp.exists("{}/{:04d}/{:03d}".format(self.folder_path,year,day)):
+            logging.info("mesoDB/year/month - Existent DB path {}/{:04d}/{:03d}".format(self.folder_path,year,day))
+            return True
         else:
-            os.makedirs("{}/{:04d}/{:02d}".format(self.folder_path,year,month))
+            os.makedirs("{}/{:04d}/{:03d}".format(self.folder_path,year,day))
+            return False
     
     # Checks if day file exits, if so returns true, else false
     #
-    def day_file_exists(self,day,month,year):
-        if osp.exists("{}/{:4d}/{:02d}/{:02d}.pkl".format(self.folder_path,year,month,day)):
-            logging.info("mesoDB - Existent DB path {}/{:04d}/{:02d}/{:02d}.pkl".format(self.folder_path,year,month,day))
+    def hour_file_exists(self,hour,day,year):
+        if osp.exists("{}/{:4d}/{:03d}/{:04d}{:03d}{:02d}.pkl".format(self.folder_path,year,day,year,day,hour)):
+            logging.info("mesoDB - Existent DB path {}/{:04d}/{:03d}/{:04d}{:03d}{:02d}.pkl".format(self.folder_path,year,day,year,day,hour))
             return True
         else:
             return False
-
+                
 
     # Initialize parameters for get_data
     #   
     def init_params(self):
-        self.params = {'year': [int(datetime.datetime.now().year)], 'month': [int(datetime.datetime.now().year)], 'day': [0],
+        self.params = {'year': [int(datetime.datetime.now().year)], 'month': [int(datetime.datetime.now().month)], 'day': [0],
                     'latitude1': None, 'latitude2': None, 'longitude1': None, 'longitude2': None, 'makeFile': False, 'override': False}
     
     
@@ -117,36 +122,64 @@ class mesoDB(object):
         else:
             dayLimit = 30
         return dayLimit
-    
+            
     
     # Save data from mesowest to the local database
     #
+    # @ Param df_new - dataframe with fuel moisture data
+    # @ Param day - julian calander day (0-365)
+    # @ Param year - given year
     #
-    def save_to_DB(self,df_new,day,month,year):
+    def save_to_DB(self,df_new,hour,day,year):
         
         # If the day file does not exist, make the folder and save the data
-        if self.day_file_exists(day,month,year) == False:
-            df_new.to_pickle("{}/{:04d}/{:02d}/{:02d}.pkl".format(self.folder_path,year,month,day))
+        if self.hour_file_exists(hour,day,year) == False:
+            df_new.to_pickle("{}/{:04d}/{:03d}/{:04d}{:03d}{:02d}.pkl".format(self.folder_path,year,day,year,day,hour))
         # If the day file already exists, add the new data to the original file
         else:
-            df_local = pd.read_pickle("{}/{:04d}/{:02d}/{:02d}.pkl".format(self.folder_path,year,month,day))
-            df_new_local = pd.concat(df_local,df_new).drop_duplicates().reset_index(drop=True)
-            df_new_local.to_pickle("{}/{:04d}/{:02d}/{:02d}.pkl"+day.format(self.folder_path,year,month,day))
+            # If the day file already exists, append the new data to the original file
+            df_local = pd.read_pickle("{}/{:04d}/{:02d}/{:04d}{:03d}{:02d}.pkl".format(self.folder_path,year,day,year,day,hour))
+            df_new_local = pd.concat([df_local,df_new]).drop_duplicates().reset_index(drop=True)
+            df_new_local.to_pickle("{}/{:04d}/{:02d}/{:04d}{:03d}{:02d}.pkl".format(self.folder_path,year,day,year,day,hour))
     
     
-    def get_and_save(self,mesowestData,day,month,year,siteList,fuelList,datesList,latList,lonList,stateList):
+    # Pull fuel moisture data from mesowest dictionary, update the station.pkl file, and save the mesowest data to pickle files
+    #
+    # @ Param mesowestData - mesowest data dictionary
+    # @ Param day - day of a given month
+    # @ Param month - month of a given year
+    # @ Param year - given year
+    #
+    def get_and_save(self,mesowestData,day,month,year):
         # Fills data from mesowest into lists
-        for i in range(len(mesowestData['STATION'])):
-            for j in range(len(mesowestData['STATION'][i]['OBSERVATIONS']['date_time'])):
-                siteList.append(mesowestData['STATION'][i]['NAME'])
-                fuelList.append(mesowestData['STATION'][i]['OBSERVATIONS']['fuel_moisture_set_1'][j])
-                datesList.append(mesowestData['STATION'][i]['OBSERVATIONS']['date_time'][j])
-                latList.append(mesowestData['STATION'][i]['LATITUDE'])
-                lonList.append(mesowestData['STATION'][i]['LONGITUDE'])
-                stateList.append(mesowestData['STATION'][i]['STATE'])
-                    
-        df_new = pd.DataFrame({'Name': siteList, 'dateTime': datesList, 'Latitude':latList, 'Longitude': lonList, 'State': stateList, 'Fuel Moisture': fuelList})
-        self.save_to_DB(df_new,day,month,year)
+        data_file = pd.DataFrame([])
+        day = datetime.datetime(year,month,day,tzinfo=datetime.timezone.utc).timetuple().tm_yday
+        for stData in mesowestData['STATION']:
+            df = pd.DataFrame.from_dict(stData['OBSERVATIONS'])
+            df.columns = ['datetime','fm10']
+            df['STID'] = stData['STID']
+            data_file = data_file.append(df)
+        
+        # Creates/updates the station.pkl file which holds constant values for each station (STID, elevation, latitude, longitude, etc.)
+        if osp.exists("{}/{}".format(self.folder_path,'station.pkl')):
+            sts_pd = pd.read_pickle("{}/{}".format(self.folder_path,'station.pkl'))    
+        # Create empty dataframe for station metadata
+        else:
+            sts_pd = pd.DataFrame([])
+        keys = ['STID','LONGITUDE','LATITUDE','ELEVATION','STATE']
+        stids = np.array([station['STID'] for station in mesowestData['STATION']])
+        np_meso = np.array(mesowestData['STATION'])
+        sts_miss = np_meso[~np.isin(stids,sts_pd.index)]
+        sts_pd = sts_pd.append(pd.DataFrame.from_dict({key: [ms[key] for ms in sts_miss] for key in keys}).set_index('STID'))
+        sts_pd.to_pickle("{}/{}".format(self.folder_path,'station.pkl'))   
+        
+        # Save data for each hour of a given day
+        data_file['datetime'] = pd.to_datetime(data_file['datetime'])
+        for hour in range(0,24):
+            hourData = data_file[data_file['datetime'].dt.hour == hour]
+            if len(hourData) > 0:
+                hourData = hourData.reset_index()
+                self.save_to_DB(hourData,hour,day,year)
     
     
     # If the user does not specify the days they want, gets the last month's data
@@ -162,7 +195,7 @@ class mesoDB(object):
     def getMonthlyData(self,token,dayLimit,month,year):
         
         currentDay = 1
-        while currentDay+1 <= dayLimit:
+        while currentDay < dayLimit+1:
             self.getDailyData(token,currentDay,dayLimit,month,year)
             currentDay+=1
     
@@ -181,32 +214,26 @@ class mesoDB(object):
     #
     def getDailyData(self,token,day,dayLimit,month,year):
     
-        # Lists to hold data from mesowest
-        siteList = []
-        fuelList = []
-        datesList = []
-        latList = []
-        lonList = []
-        stateList = [] 
         override = self.params.get('override')
     
         # If it's the last day of the year, set month to 1 and year + 1. 
-        if month == 12:
-            month2 = 1
-            year2 = year+1
-        # Else, increment the month by 1
-        else:
-            month2 = month+1
-            year2 = year
-        # The following month is always 1
-        day2 = 1
+        if day == dayLimit:
+            if month == 12:
+                month2 = 1
+                year2 = year+1
+            # Else, increment the month by 1
+            else:
+                month2 = month+1
+                year2 = year
+            # The following month is always 1
+            day2 = 1
         
         # If current month, get given this month's data up until today
         if month == int(datetime.datetime.now(datetime.timezone.utc).month) and year == int(datetime.datetime.now(datetime.timezone.utc).year):
-            dayLimit = datetime.datetime.now().day
+            dayLimit = datetime.datetime.now(datetime.timezone.utc).day
             
-        # Check if the  day file exists, if true check if override parameter is true
-        if self.day_file_exists(day, month, year) == True:
+        # Check if the julian day folder exists, create it if false
+        if self.julian_exists(day,month,year) == True:
             # If override parameter is true, get data from mesowest and append any new data to existing day file (i.e. 01.pkl)
             if override == True:
                 
@@ -218,10 +245,10 @@ class mesoDB(object):
                 elif day+1 <= dayLimit:
                     mesoData = token.timeseries(start="{:04d}{:02d}{:02d}{:04d}".format(year,month,day,0), end = "{:04d}{:02d}{:02d}{:04d}".format(year,month,day+1,0),state='CA',vars='fuel_moisture')
                 
-                self.get_and_save(mesoData,day,month,year,siteList,fuelList,datesList,latList,lonList,stateList)
+                self.get_and_save(mesoData,day,month,year)
                     
             else:
-                print('{:02d}.pkl already exists'.format(day))
+                print('{:02d}/{:02d}/{:04d} file already exists'.format(month,day,year))
                 
         else:
             # if it's the last day of the month (i.e. Jan), make month2 and day2 the first day of the next month (i.e. Feb) to get all the data from the last day of the original month (i.e. Jan)
@@ -232,9 +259,10 @@ class mesoDB(object):
             elif day+1 <= dayLimit:
                 mesoData = token.timeseries(start="{:04d}{:02d}{:02d}{:04d}".format(year,month,day,0), end = "{:04d}{:02d}{:02d}{:04d}".format(year,month,day+1,0),state='CA',vars='fuel_moisture')
         
-            self.get_and_save(mesoData,day,month,year,siteList,fuelList,datesList,latList,lonList,stateList)
+            self.get_and_save(mesoData,day,month,year)
             
     
+    # Supposed to get last available hour
     def getHourly(self,token):
         
         siteList = []
@@ -295,13 +323,13 @@ class mesoDB(object):
     
     
     # Gets fuel data from Mesowest and makes it into a pickle or csv file
-    def main(self,token,days=[0],months=[int(datetime.datetime.now().month)],years=[int(datetime.datetime.now().year)],currentData=False):
+    def main(self,days=[0],months=[int(datetime.datetime.now().month)],years=[int(datetime.datetime.now().year)],currentData=False):
 
+        token = self.token
         if currentData == False:
             for year in years:
                 self.year_exists(year)
                 for month in months:
-                    self.month_exists(year, month)
                     daysInMonth = self.daysInMonth(month,year)
                     if days[0] == 0:
                         self.getMonthlyData(token,daysInMonth,month,year)
@@ -312,7 +340,7 @@ class mesoDB(object):
                         print('Invalid Day Input')
                     
         else:
-            print('Need to set up for hourly')
+            print('Note to editor: Need to set up for hourly')
             self.year_exists(year)
             self.month_exists(year, month)
             self.getHourly(token)
@@ -339,48 +367,70 @@ class mesoDB(object):
             lon2 = longitude1
         return lat1,lat2,lon1,lon2
     
+    
     # Gets mesowest data from local database
+    #
     def get_db(self): 
-        year = self.params.get('year')
-        month = self.params.get('month')
-        day = self.params.get('day')
-        lat1 = self.params.get('latitude1')
-        lat2 = self.params.get('latitude2')
-        lon1 = self.params.get('longitude1')
-        lon2 = self.params.get('longitude2')
+        # Load parameters for getting data
+        years = self.params.get('year')
+        months = self.params.get('month')
+        days = self.params.get('day')
+        latitude1 = self.params.get('latitude1')
+        latitude2 = self.params.get('latitude2')
+        longitude1 = self.params.get('longitude1')
+        longitude2 = self.params.get('longitude2')
         makeFile = self.params.get('makeFile')
         
-        lat1,lat2,lon1,lon2 = self.check_coords(lat1, lat2, lon1, lon2)
-        for i in year:
-            for j in month:
-                multipleDays = False
-                for k in day:
-                    df_local = pd.read_pickle("{}/{:04d}/{:02d}/{:02d}.pkl".format(self.folder_path,i,j,day))
-                    df_local['Latitude'] = df_local['Latitude'].astype(float)
-                    df_local['Longitude'] = df_local['Longitude'].astype(float)
-                    df_local['dateTime'] = pd.to_datetime(df_local['dateTime'])
-                    if multipleDays == False:
-                        if lat1 != None:
-                            df_new = df_local[np.logical_and(df_local['Latitude'].between(lat1, lat2, inclusive=True),df_local['Longitude'].between(lon1, lon2, inclusive=True))]
-                            if k != 0:
-                                df_new = df_new[df_new['dateTime'].dt.day.between(k-1,k+1, inclusive=True)]
-                        else:
-                            if k == 0:
-                                df_new = df_local
-                            else:
-                                df_new = df_local[df_local['dateTime'].dt.day.between(k-1,k+1, inclusive=False)]
-                    else:
-                        if lat1 != None:
-                            df_new = pd.concat([df_new,df_local[np.logical_and(df_local['Latitude'].between(lat1, lat2, inclusive=True),df_local['Longitude'].between(lon1, lon2, inclusive=True))]])
-                            if k != 0:
-                                df_new = df_new[df_new['dateTime'].dt.day.between(k-1,k+1, inclusive=True)]
-                        else:
-                            if k == 0:
-                                df_new = df_local
-                            else:
-                                df_new = df_local[df_local['dateTime'].dt.day.between(k-1,k+1, inclusive=False)]
-                    multipleDays = True
+        # Check if the coordinates are valid
+        lat1,lat2,lon1,lon2 = self.check_coords(latitude1, latitude2, longitude1, longitude2)
         
+        # Load station.pkl data and create an empty dataframe which will hold all the data requested
+        df_sites = pd.read_pickle("{}/{}".format(self.folder_path,"station.pkl"))
+        df_new = pd.DataFrame([])
+        # Look at each year given
+        for year in years:
+            # Look at each month given
+            for month in months:
+                # Day reset to make sure days is unique for each month
+                days = self.params.get('day')
+                # If "0" value provided, assume all days for a given month are queried
+                if days[0] == 0:
+                    print(month)
+                    days = list(np.arange(0,self.daysInMonth(month,year))+1)
+                # Look at each day given
+                print(days)
+                for day in days:
+                    # If the datetime given is in the future, don't do anything
+                    #print(year,month,day)
+                    if datetime.datetime(year,month,day,tzinfo=datetime.timezone.utc) <= datetime.datetime.now(datetime.timezone.utc):
+                        # Gets the julian day from the day, month, and year
+                        jday = datetime.datetime(year,month,day,tzinfo=datetime.timezone.utc).timetuple().tm_yday
+                        # Look at each hour on a given day
+                        for hour in np.arange(0,24):
+                            # If the file does not exist, get it and add it to the df_new dataframe
+                            if self.hour_file_exists(hour,jday,year) == False:
+                                temp = self.params.get('makeFile')
+                                self.params['override'] = True
+                                self.getDailyData(self.token,day,self.daysInMonth(month,year),month,year)
+                                self.params['override'] = temp
+                            
+                            # Get the queried data
+                            df_local = pd.read_pickle("{}/{:04d}/{:03d}/{:04d}{:03d}{:02d}.pkl".format(self.folder_path,year,jday,year,jday,hour))
+                            df_local['datetime'] = pd.to_datetime(df_local['datetime'])
+                            # If valid coordinates are given, get data in those bounds
+                            if lat1 != None:
+                                df_sites['LATITUDE'] = df_sites['LATITUDE'].astype(float)
+                                df_sites['LONGITUDE'] = df_sites['LONGITUDE'].astype(float)
+                                stidLoc = df_sites[np.logical_and(df_sites['LATITUDE'].between(lat1, lat2, inclusive=True),df_sites['LONGITUDE'].between(lon1, lon2, inclusive=True))].index.values
+                                df_new = df_new.append(df_local[df_local['STID'].isin(stidLoc)])
+                            # If no coordinates are given, get all the data in date range given
+                            else:
+                                df_new = df_new.append(df_local)
+                                df_new = df_new.reset_index()
+                    else:
+                        print("{} is not valid".format(datetime.datetime(year,month,day,tzinfo=datetime.timezone.utc)))
+        df_new = df_new.reset_index()
+        df_new = df_new.drop(['level_0','index'],axis=1)
         return df_new
     
     
@@ -388,11 +438,20 @@ class mesoDB(object):
 #
 if __name__ == '__main__':
     
-    # Put your token here
-    m = Meso(token='YourTokenGoesHere')
-    meso = mesoDB()
+    # Example
+    meso = mesoDB('YourTokenGoesHere')
     #meso.main(m,currentData = True) 
-    meso.main(token=m,days=[1],months=[1,2,3,4,5],years=[2021])
-    #meso.main(token=m,days=[31],months=[5],years=[2020])
+    #meso.main(days=[0],months=[12],years=[2020])
+    meso.main(days=[0],months=[5],years=[2020])
+    
+    meso.params['latitude1'] = 37
+    meso.params['latitude2'] = 38
+    meso.params['longitude1'] = -120
+    meso.params['longitude2'] = -122
+    meso.params['year'] = [2020]
+    meso.params['month'] = [1,5]
+    meso.params['day'] = [0]
+    testFrame = meso.get_db()
+    
     
     
